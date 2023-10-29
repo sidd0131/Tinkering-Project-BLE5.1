e0edc9cc49aa9257b273a5d1c608b68f28ba80e5
sidd0131 <100462584+sidd0131@users.noreply.github.com>
2023-10-29 22:47:52 +0800

    converted into platformio type

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Tinkering-Project-BLE5.1
 Exploring BLE5.1 capabilities through a person-monitoring robot car system using SiLabs Thunderboard EFR32BG22 board as the beacon, EFR32MG22 as receiver MCU attached to the antenna array.
 
-A rough workflow of the idea:
+A rough workflow:
 
 ![Alt text](image.png)
--- src/BLE5.1_RX/RX.h
+++ src/BLE5.1_RX/RX.h
@@ -5,56 +5,51 @@ public:
 
     float measure_phase_difference(float signal) {
         // Measure phase difference logic
-        // Replace with actual implementation
         return 0.0;
     }
 
     float calculate_distance(float phase_difference) {
         // Calculate distance logic
-        // Replace with actual implementation
         return 0.0;
     }
 
     float estimate_arrival_angle(float distance) {
-        // Estimate arrival angle logic
-        // Replace with actual implementation
+        // Estimate arrival angle
         return 0.0;
     }
 
     void update_beacon_position(float angle) {
         // Update beacon position logic
-        // Replace with actual implementation
     }
 };
 
 // Define the Microcontroller class
-class Microcontroller {
+class RX {
 public:
+
     AntennaArray antenna_array;
 
-    void initialize_microcontroller() {
+    void initialize() {
         // Initialization logic
     }
 
     float receive_signal() {
         // Receive signal logic
-        // Replace with actual implementation
         return 0.0;
     }
 
     void perform_operations_based_on_angle(float angle) {
         // Operations based on angle logic
-        // Replace with actual implementation
     }
 
     void add_delay() {
         // Delay logic
-        // Replace with actual implementation
     }
 
-    void run_algorithm() {
-        initialize_microcontroller();
+    void run_pdoa() {
+        initialize();
         while (true) {
+
             float received_signal = receive_signal();
             float phase_difference = antenna_array.measure_phase_difference(received_signal);
             float distance = antenna_array.calculate_distance(phase_difference);
@@ -67,12 +62,16 @@ public:
 };
 
 // Usage
-Microcontroller microcontroller;
+RX rx;
 
 void setup() {
-    // Setup logic
+
+    // Setup
+
 }
 
 void loop() {
-    microcontroller.run_algorithm();
+
+    rx.run_pdoa();
+
 }
